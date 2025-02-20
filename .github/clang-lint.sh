71de4753234f23315ad51c4d1cfe1ba9a4f0023c
Holger Müller <github@euhm.de>
2025-02-20 14:55:14 +0100

    test

--- .github/clang-lint.sh
+++ .github/clang-lint.sh
@@ -6,7 +6,8 @@ shopt -s globstar
 # Make sure we are inside the github workspace
 cd $GITHUB_WORKSPACE
 # Install clang-format (if not already installed)
-command -v clang-format >/dev/null 2>&1 || sudo apt-get -y install clang-format
+#command -v clang-format >/dev/null 2>&1 || sudo apt-get -y install clang-format
+command -v clang-format || sudo apt-get -y install clang-format
 # Check clang-format output
 for f in **/*.{h,c,hpp,cpp,ino} ; do
     if [ -f "$f" ] && [[ "$f" != "tests/"* ]]; then
