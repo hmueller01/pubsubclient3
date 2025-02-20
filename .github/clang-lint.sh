7b2d53dbcbc5b633c4c1fb2dbb9efa813b9c3d60
Holger Müller <github@euhm.de>
2025-02-20 14:36:18 +0100

    remove tests/ folder from checking

--- .github/clang-lint.sh
+++ .github/clang-lint.sh
@@ -5,12 +5,15 @@ set -e
 shopt -s globstar
 # Make sure we are inside the github workspace
 cd $GITHUB_WORKSPACE
-# Install clang-format
-sudo apt-get -y install clang-format
+# Install clang-format (if not already installed)
+command -v clang-format >/dev/null 2>&1 || sudo apt-get -y install clang-format
+set +e
 # Check clang-format output
 for f in **/*.{h,c,hpp,cpp,ino} ; do
-    if [ -f "$f" ]; then
+    if [ -f "$f" ] && [[ "$f" != "tests/"* ]]; then
+        echo "################################################################"
         echo "Checking file ${f}"
         diff $f <(clang-format -assume-filename=main.cpp $f) 1>&2
+        echo -e "################################################################\n"
     fi
 done
