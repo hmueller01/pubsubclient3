9e152c31f537bb3301a68d5f780a5e668f2bb402
Holger Müller <github@euhm.de>
2025-02-24 21:17:19 +0100

    updated build-arduino.sh

--- .github/build-arduino.sh
+++ .github/build-arduino.sh
@@ -14,12 +14,16 @@ curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.
 arduino-cli config init
 arduino-cli config add board_manager.additional_urls http://arduino.esp8266.com/stable/package_esp8266com_index.json
 arduino-cli core update-index
-arduino-cli core search
+#arduino-cli core search
 # Install Arduino cores
 #arduino-cli core install arduino:avr
 arduino-cli core install esp8266:esp8266
 arduino-cli core install esp32:esp32
-arduino-cli board listall
+#arduino-cli board listall
+# Install Arduino libs
+arduino-cli lib update-index
+arduino-cli lib upgrade
+arduino-cli lib install Ethernet
 # Link the project to the Arduino library
 ln -s $GITHUB_WORKSPACE $HOME/Arduino/libraries/CI_Test_Library
 # Compile all *.ino files for the Arduino Uno
