1dd7471e623b3601443ecf2600cd8f6ab3a6ae3c
Holger Müller <github@euhm.de>
2026-06-14 14:54:42 +0200

    better readability of if case

--- src/PubSubClient.cpp
+++ src/PubSubClient.cpp
@@ -378,7 +378,7 @@ bool PubSubClient::handlePacket(uint8_t hdrLen, size_t length) {
                 // - Payload (for QoS > 0): length - (hdrLen + 5 + topicLen) bytes (starts at _buffer[hdrLen + 5 + topicLen])
                 // To get a null terminated 'C' topic string we move the topic 1 byte to the front (overwriting the LSB of the topic lenght)
                 // Guard 1: ensure topic length bytes are readable
-                if (hdrLen + 3ul > length) {
+                if (length < hdrLen + 3ul) {
                     DEBUG_PSC_PRINTF("handlePacket(): Packet too short to contain topic length field\n");
                     return false;
                 }
